351bf6de323d263c6f59ff44929efc57c9a6b485
Mr-Dany <49878464+Mr-Dany@users.noreply.github.com>
2021-02-10 00:15:48 -0500

    BD

---
+++ BD/Student.sql
@@ -0,0 +1,10 @@
+--drop database StudentDB
+--create database StudentDB
+--use StudentDB
+create table Student(
+StudentId int not null primary key,
+name varchar(50),
+roll varchar(50),
+); 
+CREATE PROCEDURE dbo.SP_Student
+AS
